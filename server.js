08d1950e9373a96f2076af0e9e03566466557aa4
ssura <svensura@germanscreens.de>
2019-01-04 16:58:38 +0100

    project page added

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+        pageTitle: 'Projects Page',     
+    });
+});
+
 app.get('/bad', (request, response) => {
     response.send({errorMessage: 'unable to handle request'});
 });
